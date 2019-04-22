a138d66294a95bb083ed01afa486f56eaaeb95f2
Myles Thiessen <thiessem@uwindsor.ca>
2019-04-21 20:22:14 -0600

    updated build files

--- backend/admin-site/build.sh
+++ backend/admin-site/build.sh
@@ -2,6 +2,6 @@
 if [ "$(docker inspect -f '{{.State.Running}}' prntscrn-admin-panel)" ]; then
     docker kill prntscrn-admin-panel
     docker container rm prntscrn-admin-panel
-    docker build -t react-app .
-    docker run -dit -p 3001:3001 --restart=unless-stopped --name=prntscrn-admin-panel react-app
 fi
+docker build -t react-app .
+docker run -dit -p 3001:3001 --restart=unless-stopped --name=prntscrn-admin-panel react-app
--- backend/prntscrn-api/build.sh
+++ backend/prntscrn-api/build.sh
@@ -2,6 +2,6 @@
 if [ "$(docker inspect -f '{{.State.Running}}' prntscrn-api)" ]; then
     docker kill prntscrn-api
     docker container rm prntscrn-api
-    docker build -t rails-app .
-    docker run -dit -p 3000:3000 --restart=unless-stopped --name=prntscrn-api rails-app
 fi
+docker build -t rails-app .
+docker run -dit -p 3000:3000 --restart=unless-stopped --name=prntscrn-api rails-app
