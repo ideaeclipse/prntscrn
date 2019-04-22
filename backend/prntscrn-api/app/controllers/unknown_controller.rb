95c84deb33d3d09266904d7b42a5f8b556c983c3
Myles Thiessen <thiessem@uwindsor.ca>
2019-04-21 19:21:34 -0600

    added that unknown endpoint posts url you called

--- backend/prntscrn-api/app/controllers/unknown_controller.rb
+++ backend/prntscrn-api/app/controllers/unknown_controller.rb
@@ -2,6 +2,6 @@ class UnknownController < ApplicationController
   skip_before_action :auth_user
 
   def unknown
-    render json: {status: "Unknown endpoint"}
+    render json: {status: "Unknown endpoint", url: request.fullpath}
   end
 end
