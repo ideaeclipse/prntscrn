11dd82bfb668f70cf6ac43741f28ae797c1c7ba2
Myles Thiessen <thiessem@uwindsor.ca>
2019-05-01 10:19:31 -0600

    Update install.sh

--- backend/install.sh
+++ backend/install.sh
@@ -74,7 +74,8 @@ git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
 
 #download 2.5.5
-exec bash
+-="i"
+source ~/.bashrc
 rbenv install 2.5.5
 rbenv global 2.5.5
 
