755b262e1001aa7f04e6f55560604b216fdadd06
Myles Thiessen <thiessem@uwindsor.ca>
2019-05-01 22:34:56 -0600

    updated kubernetes install to auto build and push to docker hub on install for development

--- backend/admin-site/kubernetes/install.sh
+++ backend/admin-site/kubernetes/install.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+cd ../
+sudo docker build -t react-app .
+sudo docker tag $(sudo docker images react-app --format "{{.ID}}") ideaeclipse/react-app:latest
+sudo docker push ideaeclipse/react-app
+cd kubernetes/
 kubectl apply -f pod.yaml
 kubectl apply -f service.yaml
--- backend/prntscrn-api/kubernetes/api/install.sh
+++ backend/prntscrn-api/kubernetes/api/install.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+cd ../
+sudo docker build -t rails-app .
+sudo docker tag $(sudo docker images rails-app --format "{{.ID}}") ideaeclipse/rails-app:latest
+sudo docker push ideaeclipse/rails-app
+cd kubernetes/
 kubectl apply -f pod.yaml
 kubectl apply -f service.yaml
